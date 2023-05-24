e319334e20528e73d0d5584218b92431d4bc7a20
Eunki Chung <eunkich@gmail.com>
2023-05-23 23:19:08 -0700

    wip: naive implementation of backprop

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <math.h>         // log, sqrt
 #include "naive_blas.hpp" // gemv
 
+// dataloader
 std::vector<std::vector<float>> read_csv()
 {
     std::string line;
@@ -59,17 +60,6 @@ void save_bin(int n, int m, std::vector<std::vector<float>> &data)
     data.clear();
 }
 
-template <typename T>
-void print_vector(std::vector<T> vec)
-{
-    std::cout << "[";
-    for (auto e : vec)
-    {
-        std::cout << e << ", ";
-    }
-    std::cout << "\b\b]\n";
-}
-
 void read_bin(const int &n, const int &m, std::vector<float> &out)
 {
     // Read
@@ -83,31 +73,6 @@ void read_bin(const int &n, const int &m, std::vector<float> &out)
     input.read(reinterpret_cast<char *>(out.data()), sizeof(float) * n * m);
 }
 
-void print_digit(std::vector<float> &sample)
-{
-    std::cout << "label: " << sample[0] << "\n";
-    for (int i = 0; i < 28; i++)
-    {
-        for (int j = 0; j < 28; j++)
-        {
-            std::cout << std::setw(3) << sample[1 + j + i * 28] << " ";
-        }
-        std::cout << "\n";
-    }
-}
-
-float log_softmax(int i, std::vector<float> z)
-{
-    const int n = z.size();
-    float expsum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        expsum += exp(z[i]);
-    }
-
-    return z[i] - log(expsum);
-}
-
 void load_data(int n, int m, std::vector<std::vector<float>> &samples)
 {
     std::vector<float> data(60000 * 785);
@@ -116,7 +81,11 @@ void load_data(int n, int m, std::vector<std::vector<float>> &samples)
     {
         for (int j = 0; j < 785; j++)
         {
-            samples[i][j] = data[i + j * 60000] / 255;
+            samples[i][j] = data[i + j * 60000];
+            if (j > 0)
+            {
+                samples[i][j] /= 255;
+            }
         }
     }
 }
@@ -134,21 +103,149 @@ std::vector<float> extract_label(std::vector<std::vector<float>> &samples)
     return y;
 }
 
+std::vector<float> onehot_encode(int y)
+{
+    std::vector<float> out(10, 0);
+    out[y] = 1.f;
+    return out;
+}
+
+// utils
+template <typename T>
+void print_vector(std::vector<T> vec)
+{
+    if (vec.size() == 0)
+    {
+        std::cout << "No element in vec\n";
+        return;
+    }
+
+    std::cout << "[";
+    for (auto e : vec)
+    {
+        std::cout << e << ", ";
+    }
+    std::cout << "\b\b]\n";
+}
+
+void print_digit(std::vector<float> &sample)
+{
+    std::cout << "label: " << sample[0] << "\n";
+    for (int i = 0; i < 28; i++)
+    {
+        for (int j = 0; j < 28; j++)
+        {
+            std::cout << std::setw(3) << sample[1 + j + i * 28] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+// operations
+std::vector<float> log_softmax(std::vector<float> x)
+{
+    std::vector<float> out(x.size(), 0);
+    const int n = x.size();
+    float expsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        expsum += exp(x[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        out[i] = x[i] - log(expsum);
+    }
+
+    return out;
+}
+
+std::vector<float> softmax(std::vector<float> x)
+{
+    std::vector<float> out(x.size(), 0);
+    const int n = x.size();
+    float expsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        expsum += exp(x[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        out[i] = exp(x[i]) / expsum;
+    }
+
+    return out;
+}
+
+float cross_entropy(std::vector<float> x, std::vector<float> y)
+{
+    float out = 0;
+    const int n = x.size();
+    for (int i = 0; i < n; i++)
+    {
+        out -= y[i] * log(x[i]);
+    }
+
+    return out;
+}
+
+std::vector<float> grad_z_L(std::vector<float> s, std::vector<float> y)
+{
+    std::vector<float> out(y.size(), 0);
+    for (int i = 0; i < out.size(); i++)
+    {
+        out[i] = s[i] - y[i];
+    }
+
+    return out;
+}
+
+// max(0, x) elementwise
+void relu(std::vector<float> &x)
+{
+    for (int i = 0; i < x.size(); i++)
+    {
+        if (x[i] < 0.f)
+        {
+            x[i] = 0;
+        }
+    }
+}
+
+std::vector<float> drelu(std::vector<float> &x)
+{
+    std::vector<float> out(x.size(), 0);
+    for (int i = 0; i < x.size(); i++)
+    {
+        if (x[i] > 0)
+        {
+            out[i] = x[i];
+        }
+    }
+
+    return out;
+}
+
 class Network
 {
 public:
     int dim_in;
     int dim_out;
-    std::vector<std::vector<float>> weights;
-    Network(int input_dim, int output_dim) : dim_in(input_dim), dim_out(output_dim), weights()
+    std::vector<float> _x;
+    std::vector<std::vector<float>> W;
+    std::vector<float> _a;
+    std::vector<float> _z;
+
+    Network(int input_dim, int output_dim) : dim_in(input_dim), dim_out(output_dim), W()
     {
-        weights.resize(dim_out);
+        W.resize(dim_out);
         for (int i = 0; i < dim_out; i++)
         {
-            weights[i].resize(dim_in + 1); // include bias
+            W[i].resize(dim_in + 1); // include bias
         }
 
-        std::cout << "W shape(" << weights.size() << ", " << weights[0].size() << ")\n";
+        std::cout << "W shape(" << W.size() << ", " << W[0].size() << ")\n";
     }
 
     void xavier_init()
@@ -161,27 +258,46 @@ public:
         float var = sqrt(2.f / static_cast<float>(dim_in + dim_out));
         std::normal_distribution<float> dist(0.f, var);
 
-        for (int i = 0; i < weights.size(); i++)
+        for (int i = 0; i < W.size(); i++)
         {
-            for (int j = 0; j < weights[0].size(); j++)
+            for (int j = 0; j < W[0].size(); j++)
             {
-                weights[i][j] = dist(rng);
+                W[i][j] = dist(rng);
             }
         }
     }
 
     std::vector<float> forward(std::vector<float> x)
     {
-        std::vector<float> y(dim_out, 0.f);
-        gemv(1.f, weights, x, 0.f, y);
-        return y;
+        std::vector<float> out(dim_out, 0.f);
+        _x = x;
+        gemv(1.f, W, x, 0.f, out);
+        _a = out;
+        out = softmax(out);
+        _z = out;
+
+        return out;
+    }
+
+    std::vector<std::vector<float>> backward(std::vector<float> grad)
+    {
+        std::vector<std::vector<float>> _grad(10, std::vector<float>(785, 0));
+        for (int i = 0; i < grad.size(); i++)
+        {
+            for (int j = 0; j < _x.size(); j++)
+            {
+                _grad[i][j] = grad[i] * _x[j];
+            }
+        }
+
+        return _grad;
     }
 };
 
 int main()
 {
     // Read train data in column major order
-    const int n = 1000, m = 785;
+    const int n = 10000, m = 785;
     std::vector<std::vector<float>> samples(n, std::vector<float>(m, 0));
     load_data(n, m, samples);
 
@@ -195,14 +311,50 @@ int main()
     //     print_digit(samples[i]);
     // }
 
+    // std::cout << y[0] << " ";
     std::vector<float> y = extract_label(samples);
+    std::vector<float> y_0 = onehot_encode(y[0]);
+    // print_vector(y_0);
+
     // print_vector(y);
+
+    std::vector<std::vector<float>> I(10, std::vector<float>(10, 0.f));
+    for (int i = 0; i < 10; i++)
+    {
+        I[i][i] = 1.f;
+    }
 
     Network net(784, 10);
     net.xavier_init();
-    std::vector<float> out = net.forward(samples[0]);
-    print_vector(out);
-    float loss = log_softmax(y[0], out);
-    std::cout << loss << "\n";
-    // std::cout << net.weights.size() << net.weights[0].size() << std::endl;
+
+    const int MINI_BATCH_SIZE = 1000;
+    std::vector<float> out, grad, y_i, x_i;
+    std::vector<std::vector<float>> _grad;
+    float loss, loss_mean;
+    float lr = 0.005;
+
+    int sp = 0, idx = 0;
+    for (int b = 0; b < 10; b++)
+    {
+        loss_mean = 0;
+        std::vector<std::vector<float>> _grad_mean(10, std::vector<float>(785, 0));
+
+        for (int i = 0; i < MINI_BATCH_SIZE; i++)
+        {
+            idx = b * MINI_BATCH_SIZE + i;
+            y_i = onehot_encode(y[idx]);
+            x_i = samples[idx];
+            out = net.forward(x_i);
+            loss = cross_entropy(out, y_i);
+            loss_mean += loss;
+
+            grad = grad_z_L(out, y_i);
+            _grad = net.backward(grad);
+            gemm(1.f / MINI_BATCH_SIZE, I, _grad, 1.f, _grad_mean);
+        }
+
+        loss_mean /= MINI_BATCH_SIZE;
+        gemm(lr, I, _grad_mean, 1.f, net.W);
+        std::cout << "mean loss: " << loss_mean << std::endl;
+    }
 }
